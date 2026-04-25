484c00827cec5d0834f4bb3e8e20ebd8187594a0
Mike <69970540+mikerb95@users.noreply.github.com>
2026-04-25 08:21:48 -0500

    Inicio de repo con boilerplate de Springintializr

---
+++ src/main/java/com/clasedominio/ClasedominioApplication.java
@@ -0,0 +1,13 @@
+package com.clasedominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasedominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasedominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clasedominio/ClasedominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clasedominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasedominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
