06d25e066bc02da1666dbb1d20154e154a7f16fe
Mike <69970540+mikerb95@users.noreply.github.com>
2026-04-25 09:24:00 -0500

    agrego archivo data.sql con inserciones iniciales para las tablas perfil y usuario

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO perfil (nombre) VALUES ('administracion');
+INSERT INTO perfil (nombre) VALUES ('secretaria');
+INSERT INTO perfil (nombre) VALUES ('vendedor');
+INSERT INTO usuario (nombre, email, id_perfil) VALUES ('Juan', 'juan@mail.com', 1);
+INSERT INTO usuario (nombre, email, id_perfil) VALUES ('Ana', 'ana@mail.com', 2);
