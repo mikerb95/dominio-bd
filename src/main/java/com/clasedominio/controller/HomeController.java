940d39e38d316ba1c824bfc4755ab10bc3604e5b
Mike <69970540+mikerb95@users.noreply.github.com>
2026-04-25 08:33:26 -0500

    actualizacion de imoprts en HomeController

--- src/main/java/com/clasedominio/controller/HomeController.java
+++ src/main/java/com/clasedominio/controller/HomeController.java
@@ -4,6 +4,8 @@ import com.clasedominio.domain.Individuo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController { // Nombre de clase debe coincidir con el archivo
